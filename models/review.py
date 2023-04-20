af4c2c910fe0b7695add650733349a2cb2acb55b
ndiritu-prisca <priscandiritu@outlook.com>
2023-04-20 20:20:30 +0000

    Changes to console

--- models/review.py
+++ models/review.py
@@ -2,6 +2,7 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Float
+from os import getenv
 
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
@@ -9,8 +10,8 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
+    __tablename__ = "reviews"
     if storage_type == 'db':
-        __tablename__ = "reviews"
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         text = Column(String(1024), nullable=False)
