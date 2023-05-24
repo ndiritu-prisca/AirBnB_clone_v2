2c241c09f2e29330e5cf7ec30e3b2296a78bed40
ndiritu-prisca <priscandiritu@outlook.com>
2023-05-24 11:32:32 -0700

    Is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    A script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """A function that returns hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A function that diplays HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """A function that diplays C followed by value of text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python(text="is cool"):
+    """A function that diplays Python followed by value of text"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_num(n):
+    """A function that displays n is a number only if it is an integer"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
